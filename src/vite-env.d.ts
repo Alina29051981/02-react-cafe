03e87696c3965bb29d297fa2c9991bbe170a5aac
Alina Kuchma <ostaliva@ukr.net>
2025-09-02 14:20:19 +0300

    add fix

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,9 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VERCEL: string; // Vercel передає "1" при збірці
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
