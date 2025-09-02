7bf1d5fd3b02af13f6a62e0ec06ab067c2b73bcb
Alina Kuchma <ostaliva@ukr.net>
2025-09-02 14:50:22 +0300

    add changes

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,2 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VERCEL: string; // Vercel передає "1" при збірці
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.VERCEL ? './' : '/02-react-cafe/', 
   build: {
     sourcemap: true,
   },
-})
+});
